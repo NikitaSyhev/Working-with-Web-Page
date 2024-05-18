6eac5b705c626dcfe0500f4a9f3985b8184c2519
Nikita Sychev <nikitasychev@inbox.ru>
2024-05-18 18:34:52 +0500

    add checkbox and delete movies

--- js/script.js
+++ js/script.js
@@ -11,40 +11,28 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', ()=> {
-    const movieDB = {
-        movies: [
-            "Логан",
-            "Лига справедливости",
-            "Ла-ла лэнд",
-            "Одержимость",
+const movieDB = {
+    movies: [
+            "Люди ИКС",
+            "Хало",
+            "Гари Поттер",
+            "Немезида",
             "Андрей",
-        ]
-    };
+    ]
+  };
     
-    const adv = document.querySelectorAll('.promo__adv img'),
-          genre = document.querySelector('.promo__genre'),
-          bcgPicture = document.querySelector('.promo__bg'),
-          listOfMovies = document.querySelector('.promo__interactive-list'),
-          formAddingImput = document.querySelector('.adding__input'),
-          buttonConfirn = document.querySelector('button');
+const adv = document.querySelectorAll('.promo__adv img'),
+    genre = document.querySelector('.promo__genre'),
+        bcgPicture = document.querySelector('.promo__bg'),
+        listOfMovies = document.querySelector('.promo__interactive-list'),
+        formAddingImput = document.querySelector('.adding__input'),
+        buttonConfirn = document.querySelector('button'),
+        checkbox = document.querySelector('[type="checkbox"]');
+
     
-    //задание 1
-    adv.forEach(item=> {
-        item.remove();
-    })
-    //задание 2
-    genre.innerHTML = 'ДРАМА';
-    
-    // задание 3
-    bcgPicture.style.backgroundImage = 'url(img/bg.jpg)';
-    
-    //задание 4
-    
-    listOfMovies.innerHTML = "";
-    
-    movieDB.movies.sort();
-    
-    function createListOfMovies () {
+//функция создания списка фильмов
+function createListOfMovies () {
+        movieDB.movies.sort();
         movieDB.movies.forEach ((film, i) => {
             listOfMovies.innerHTML += `
             <li class="promo__interactive-item">${i + 1} ${film}
@@ -52,20 +40,48 @@ document.addEventListener('DOMContentLoaded', ()=> {
             </li>
             `;
         });
-    }
-    createListOfMovies();
-   
-    
-    buttonConfirn.addEventListener('click', (e)=> {
+    //обработчик удаления фильмов
+    document.querySelectorAll('.delete').forEach((btn,i) => {
+        btn.addEventListener('click', ()=> {
+            btn.parentElement.remove();
+            movieDB.movies.splice(i, 1);
+            listOfMovies.innerHTML = "";
+            createListOfMovies();
+        })
+    })
+}
+
+//обработчик добавления фильмов
+buttonConfirn.addEventListener('click', (e)=> {
         e.preventDefault();
         let movie = formAddingImput.value;
+        let favoriteMovies = checkbox.checked;
+        
+        if(movie) {
         if (movie.length > 21) {
-            movie = movie.substring(0, 21) + '...';
+            movie = movie.substring(0, 20) + '...';
+        }
+        if(favoriteMovies) {
+            alert('Вы отметили любимый фильм !');
         }
         movieDB.movies.push(`${movie}`);
         listOfMovies.innerHTML = "";
         createListOfMovies();
-    })
+    }
+})
+
+//задание 1
+adv.forEach(item=> {
+        item.remove();
+})
+//задание 2
+genre.innerHTML = 'ДРАМА';
+// задание 3
+bcgPicture.style.backgroundImage = 'url(img/bg.jpg)';
+//задание 4
+listOfMovies.innerHTML = "";
+    
+createListOfMovies();   
 })
 
 
